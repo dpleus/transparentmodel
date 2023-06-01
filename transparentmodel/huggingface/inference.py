7365d67de76f00090dc071c7b82a3231351b67d6
dpleus <danielpleus@hotmail.com>
2023-06-01 20:09:55 +0200

    fixed inference

--- transparentmodel/huggingface/inference.py
+++ transparentmodel/huggingface/inference.py
@@ -3,12 +3,14 @@ from transparentmodel.analysis import model_size, memory_in_use, available_ram
 from torch.profiler import profile, record_function, ProfilerActivity
 import threading
 import json
+import torch
 
 from transparentmodel.analysis import capture_memory_usage
 
 
 def track_memory_usage(func):
-    def wrapper(model, input_token, *args, **kwargs):
+    def wrapper(model, realtime, *args, **kwargs):
+
         print("---------- System memory ----------")
         print(f"Memory available: {json.dumps(available_ram(), indent=4)}")
         print(f"Memory used by process: {memory_in_use()} ")
@@ -20,29 +22,36 @@ def track_memory_usage(func):
 
         print("")
         print("---------- Memory usage ----------")
-        stop_event = threading.Event()
 
-        memory_thread = threading.Thread(target=capture_memory_usage, args=(stop_event,))
-        memory_thread.start()
+        if realtime:
+            stop_event = threading.Event()
+
+            memory_thread = threading.Thread(target=capture_memory_usage, args=(stop_event,))
+            memory_thread.start()
 
         with profile(activities=[ProfilerActivity.CPU], record_shapes=True, profile_memory=True) as prof:
             with record_function("func"):
-                model_output = func(model, input_token, *args, **kwargs)
-                stop_event.set()
+                model_output = func(model, *args, **kwargs)
+                if realtime:
+                    stop_event.set()
 
-        memory_thread.join()
+        if realtime:
+            memory_thread.join()
 
         print("")
         print("---------- Detailed Information ----------")
         print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
         print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
 
+        if torch.cuda.is_available():
+            print(prof.key_averages().table(sort_by="gpu_time_total", row_limit=10))
+            print(prof.key_averages().table(sort_by="self_gpu_memory_usage", row_limit=10))
         return model_output
 
     return wrapper
 
 
 @track_memory_usage
-def generate_with_memory_tracking(model, input_token, *args, **kwargs):
-    output = model.generate(input_token, **kwargs)
+def generate_with_memory_tracking(model, realtime=True, *args, **kwargs):
+    output = model.generate(**kwargs)
     return output
